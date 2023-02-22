019611b50eb2cf3a8c3a2553bb93044dc7c8d17e
Soumajitroy01 <soumajitroy01@gmail.com>
2023-02-22 11:37:40 +0530

    pinvent-backend

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ app.use(cookieParser());
 app.use(express.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(cors({
-    origin: ["http://localhost:3000", "https://pinvent-app.vercel.app"],
+    origin: ["http://localhost:3000", "https://pinvent-app.web.app"],
     credentials: true
 }));
 
